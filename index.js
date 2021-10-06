307867d4d6a4cbdb29ee54e1cc8ae3f67b5b48ca
khalia <khaliaparris01@gmail.com>
2021-10-06 14:44:49 +0000

    finshed code challenge

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 //1
-function absoluteValue(){
-  
-}
+function absoluteValue(number){
+    if(typeof number === "number"){
+        if(number < 0){
+            return (number *= -1)
+        }else {
+            return number
+        }
+    }else {
+        return null
+    }
+        }
+    
 
+console.log(absoluteValue(-10))
 
 
 
